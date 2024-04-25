2b0afaf21132b95c24372632c276bae60845fb21
emmanuel <emmanuel@whetztone.net>
2024-04-25 22:52:10 +0100

    Amended Download module.

--- src/Download.hs
+++ src/Download.hs
@@ -4,10 +4,13 @@ import Prelude hiding ( writeFile )
 import Data.ByteString qualified as Bytes
 import Network.HTTP.Simple ( httpBS, getResponseBody )
 
+--file :: IO ()
+file uri filename = do
+    response <- httpBS uri
+    Bytes.writeFile filename (getResponseBody response)
+    putStrLn "File has been downloaded and saved."
+
 mrs_dymond :: IO ()
-mrs_dymond = do
-    response <- httpBS "https://archive.org/stream/mrsdymond01ritcgoog/mrsdymond01ritcgoog_djvu.txt"
-    Bytes.writeFile "mrs-dymond.txt" (getResponseBody response)
-    putStrLn "File has been nownloaded and saved."
+mrs_dymond = file "https://archive.org/stream/mrsdymond01ritcgoog/mrsdymond01ritcgoog_djvu.txt" "mrs-dymond.txt"
 
 
